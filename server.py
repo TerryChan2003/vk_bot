763c88f99b97f22ccead11301977bfad972d3f1d
Aleksandr Kharitonov <aleksandr_h2001@mail.ru>
2019-07-17 12:22:08 +0300

    update

--- server.py
+++ server.py
@@ -38,6 +38,7 @@ def timing_messages(chat_id, from_id):
     time.sleep(1)
     global tmp_chat_msg
     v = tmp_chat_msg[chat_id][from_id]
+    print(v)
     if v > msg_limits:
         sendmessage_chat(2, f"Притормозите базар в конференции {k} пользователь @id{from_id} пишет {v} сообщений в секунду!")
     del tmp_chat_msg[chat_id][from_id]
